3bb2120c06302141d76f670bc0840c5b58caf5c4
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-12-04 14:35:04 -0500

    Added Validations for Project Dates

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,30 @@
 class Project < ActiveRecord::Base
 
+	validate :ensure_dates
+
 	belongs_to :owner, class_name: 'User'
 	has_many :backers, through: :pledges, class_name: 'User'
 	has_many :rewards
-	has_many :pledges
-
 	accepts_nested_attributes_for :rewards
+
+	
+
+
+private
+
+def ensure_dates	
+	if (start_date > end_date)
+		errors.add(:end_date,": You Can't End Funding Before Beginning.")
+	end
+
+	if (start_date <= Time.now)
+		errors.add(:start_date,": Funding cannot begin in the past.")
+	end
+
+	if (end_date <= Time.now)
+		errors.add(:end_date,": Funding cannot end in the past!")
+	end
+end
+
+
 end
